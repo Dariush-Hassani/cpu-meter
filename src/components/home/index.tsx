a0eb56f8ea6f77434fd45de10e98494583eb11bd
dariush-hassani <dariush.hassani@outlook.com>
2025-09-20 03:05:38 +0330

    modify layout

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -20,16 +20,15 @@ const HomeComponent = () => {
         </div>
         <div className="min-w-[140px] max-w-[140px] h-full dark:bg-back-dark dark:border-border-dark border-1 rounded-lg"></div>
       </div>
-      <div style={{ height: "calc(100% - 400px)" }} className="flex gap-2">
+      <div style={{ height: "calc(100% - 450px)" }} className="flex gap-2">
         <div className="w-1/2 h-full  flex flex-col gap-2">
-          <div className="w-full h-1/2 dark:bg-back-dark dark:border-border-dark border-1 rounded-lg"></div>
-          <div className="w-full h-1/2 dark:bg-back-dark dark:border-border-dark border-1 rounded-lg"></div>
+          <div className="w-full h-full dark:bg-back-dark dark:border-border-dark border-1 rounded-lg"></div>
         </div>
         <div className="w-1/2 h-full  flex flex-col gap-2">
-          <div className="w-full h-1/2 dark:bg-back-dark dark:border-border-dark border-1 rounded-lg"></div>
-          <div className="w-full h-1/2 dark:bg-back-dark dark:border-border-dark border-1 rounded-lg"></div>
+          <div className="w-full h-full dark:bg-back-dark dark:border-border-dark border-1 rounded-lg"></div>
         </div>
       </div>
+      <div className="h-[40px] dark:bg-back-dark dark:border-border-dark border-1 rounded-lg"></div>
     </div>
   );
 };
